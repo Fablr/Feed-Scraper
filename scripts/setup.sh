353c9f6819184ec728062361afc55a39f56beb62
Chris Day <cday@fabler.fm>
2015-11-04 20:14:37 -0800

    Fixing setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,6 @@ xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install pyenv
 brew install pyenv-virtualenv
-brew install unixodbc
-brew install freetds --with-unixodbc
-brew install postgresql
-brew install openssl
 pyenv install 3.4.3
 pyenv rehash
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
@@ -14,4 +10,5 @@ echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
 exec $SHELL
 pyenv global 3.4.3
 pyenv virtualenv VFABLER_FEEDSCRAPER
+pyenv activate VFABLER_FEEDSCRAPER
 pip install -r requirements.txt
